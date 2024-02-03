c56acc003d8327e4e99a062281211b125f6e2fbb
Onur Ravli <onur@onurravli.com>
2024-02-03 23:03:52 +0300

    feat(frontend): create an empty next.js project

---
+++ packages/frontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+import React from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
