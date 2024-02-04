fc2acf50bf2283aebe16776a3a1cfcd606ec79e4
Onur Ravli <onur@onurravli.com>
2024-02-04 10:30:15 +0300

    chore(backend): export verifyJwt util

--- packages/backend/src/utils/index.ts
+++ packages/backend/src/utils/index.ts
@@ -1,5 +1,6 @@
 import { env } from './env.util';
 import { generateJwt } from './generateJwt.util';
 import { hashPassword } from './hashPassword.util';
+import { verifyJwt } from './verifyJwt.util';
 
-export { env, generateJwt, hashPassword };
+export { env, generateJwt, hashPassword, verifyJwt };
