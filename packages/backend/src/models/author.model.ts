f143166b306310317883fb9f8a5082c9b4a25a47
Onur Ravli <onur@onurravli.com>
2024-02-04 09:37:27 +0300

    feat(backend): create user model

---
+++ packages/backend/src/models/author.model.ts
@@ -0,0 +1,21 @@
+import type { ObjectId } from 'mongodb';
+
+class Author {
+  constructor(
+    public _id: ObjectId,
+    public username: string,
+    public email: string,
+    public password: string,
+    public entries: ObjectId[], // TODO: Will be an array of Entry model
+    public isVerified: boolean,
+  ) {
+    this._id = _id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.entries = entries;
+    this.isVerified = isVerified;
+  }
+}
+
+export { Author };
