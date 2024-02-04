a711876943a8d39c7bb8dc3351a670e125dbf0cb
Onur Ravli <onur@onurravli.com>
2024-02-04 09:32:13 +0300

    feat(backend): update POST /author controller

--- packages/backend/src/controllers/author.controller.ts
+++ packages/backend/src/controllers/author.controller.ts
@@ -5,9 +5,9 @@ import { mongodb } from '../services/mongodb.service';
 class AuthorController {
   public async get(req: Request, res: Response) {
     try {
-      const { nickname } = req.params;
+      const { username } = req.params;
       const author = await mongodb.collections.authors?.findOne({
-        nickname: nickname,
+        username: username,
       });
       if (author) {
         return res.status(200).json(author);
@@ -24,10 +24,26 @@ class AuthorController {
   }
   public async post(req: Request, res: Response) {
     try {
-      const { nickname, email, password } = req.body;
+      const { username, email, password } = req.body;
+      const isUsernameInUse = await mongodb.collections.authors?.findOne({
+        username: username,
+      });
+      if (isUsernameInUse) {
+        return res.status(409).json({
+          error: 'This username is taken.',
+        });
+      }
+      const isMailInUse = await mongodb.collections.authors?.findOne({
+        email: email,
+      });
+      if (isMailInUse) {
+        return res.status(409).json({
+          error: 'This e-mail address is in use.',
+        });
+      }
       const hashedPassword = await hashPassword(password);
       await mongodb.collections.authors?.insertOne({
-        nickname: nickname,
+        username: username,
         email: email,
         password: hashedPassword,
         entries: [],
