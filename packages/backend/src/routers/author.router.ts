a441ec933049991af7b36b815190dc17dadbb287
Onur Ravli <onur@onurravli.com>
2024-02-04 10:12:53 +0300

    feat(backend): update auther router to add DELETE /author/:id

--- packages/backend/src/routers/author.router.ts
+++ packages/backend/src/routers/author.router.ts
@@ -6,5 +6,6 @@ const authorController: AuthorController = new AuthorController();
 
 authorRouter.get('/:username', authorController.get);
 authorRouter.post('/', authorController.post);
+authorRouter.delete('/:username', authorController.delete);
 
 export { authorRouter };
